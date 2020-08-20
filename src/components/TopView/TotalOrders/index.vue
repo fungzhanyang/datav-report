16e59e1bf6d391494e76251e7b65c793cfbd24dc
fengzhanyang <fungzhanyang@163.com>
2020-08-20 11:00:48 +0800

    累计订单量绘制完成

--- src/components/TopView/TotalOrders/index.vue
+++ src/components/TopView/TotalOrders/index.vue
@@ -1,11 +1,58 @@
 <template>
-  <common-card />
+  <common-card title="累计订单量" value="200,000">
+    <template>
+      <div
+        id="total-orders-chart"
+        :style="{ width: '100%', height: '100%' }"
+      ></div>
+    </template>
+    <template v-slot:footer>
+      <span>昨日订单量</span>
+      <span class="emphasis">2,000</span>
+    </template>
+  </common-card>
 </template>
 
 <script>
 import commonCardMixin from '@/mixins/commonCardMixin'
 export default {
-  mixins: [commonCardMixin]
+  mixins: [commonCardMixin],
+  mounted () {
+    const chartDom = document.querySelector('#total-orders-chart')
+    const chart = this.$echarts.init(chartDom)
+    chart.setOption({
+      xAxis: {
+        type: 'category',
+        show: false,
+        boundaryGap: false
+      },
+      yAxis: {
+        show: false
+      },
+      series: [
+        {
+          type: 'line',
+          data: [333, 100, 200, 300, 150, 222, 310, 510, 346, 153, 555],
+          areaStyle: {
+            color: 'purple'
+          },
+          lineStyle: {
+            width: 0
+          },
+          itemStyle: {
+            opacity: 0
+          },
+          smooth: true
+        }
+      ],
+      grid: {
+        top: 0,
+        bottom: 0,
+        left: 0,
+        roght: 0
+      }
+    })
+  }
 }
 </script>
 
